38581c1972d658b9c3e52c9d55a415ea0ea0a4d6
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-17 18:11:02 +0200

    Add a solution

--- src/P02_SetsOfElements.java
+++ src/P02_SetsOfElements.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class P02_SetsOfElements {
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class P02_SetsOfElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int sizeFirstSet = Integer.parseInt(input.split("\\s+")[0]);
+        int sizeSecondSet = Integer.parseInt(input.split("\\s+")[1]);
+
+        Set<Integer> firstSet = new LinkedHashSet<>();
+        Set<Integer> secondSet = new LinkedHashSet<>();
+
+        for (int i = 1; i <= sizeFirstSet ; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            firstSet.add(number);
+
+        }
+
+        for (int i = 1; i <= sizeSecondSet ; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            secondSet.add(number);
+
+        }
+
+        Set<Integer> duplicateNumbers = new LinkedHashSet<>();
+        for (int number:firstSet) {
+            if(secondSet.contains(number)) {
+                duplicateNumbers.add(number);
+            }
+        }
+        for (int number:duplicateNumbers) {
+            System.out.print(number + " ");
+        }
+
+    }
 }
