185e6ac4be42d2eb78f9dae5069d8ae0ee5682b5
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-17 17:26:08 +0200

    Add a solution

--- src/P01_UniqueUsernames.java
+++ src/P01_UniqueUsernames.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class P01_UniqueUsernames {
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class P01_UniqueUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> usernames = new LinkedHashSet<>();
+
+        int countNames = Integer.parseInt(scanner.nextLine());
+
+        for (int count = 0; count < countNames; count++) {
+            String username = scanner.nextLine();
+
+            usernames.add(username);
+        }
+        usernames.forEach(System.out::println);
+
+    }
 }
