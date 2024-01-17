9546b1ebc40165068bbbc38edfec18c128021ed3
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-17 21:31:58 +0200

    Add a solution

--- src/P03_PeriodicTable.java
+++ src/P03_PeriodicTable.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class P03_PeriodicTable {
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class P03_PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Set<String> chemicals = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String inputLine = scanner.nextLine();
+
+            String[] chemicalElements = inputLine.split("\\s+");
+            for (String element:chemicalElements) {
+                chemicals.add(element);
+
+            }
+        }
+        for (String element:chemicals) {
+            System.out.print(element + " ");
+        }
+
+    }
 }
