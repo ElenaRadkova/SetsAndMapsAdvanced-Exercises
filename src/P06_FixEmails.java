ccf7017f0f9c252d146044e3d02a7700cbcf6140
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-19 20:00:25 +0200

    Add a solution

--- src/P06_FixEmails.java
+++ src/P06_FixEmails.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class P06_FixEmails {
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P06_FixEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> emailsMap = new HashMap<>();
+
+        String name = scanner.nextLine();
+
+        while(!name.equals("stop")) {
+
+            String email = scanner.nextLine();
+
+            String emailMainDomain = email.split("@")[1];
+
+            String domain = emailMainDomain.split("\\.")[1];
+
+            emailsMap.put(name, email);
+
+            if(domain.equals("us") || domain.equals("uk") || domain.equals("com")) {
+                  emailsMap.remove(name, email);
+            } else {
+                System.out.printf("%s -> %s%n", name, emailsMap.get(name));
+            }
+
+            name = scanner.nextLine();
+        }
+
+    }
 }
